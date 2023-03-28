bec20aaf71c771c8e023af7599efe596ea72e78d
Ashik <helloashikul4@gmail.com>
2023-03-29 01:26:19 +0600

    grand total done

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Cart = ({cart}) => {
+
+    let totalPrize = 0 ;
+    let totalShipping = 0;
+    for (const product of cart) {
+        totalPrize = totalPrize + product.price 
+        totalShipping =totalShipping + product.shipping
+    }
+
+    const tax = totalPrize *7/100;
+    const grandTotal = totalPrize +totalShipping+tax
+    return (
+        <div className='bg-[#FF99004D] p-5 text-lg h-[5%] sticky top-0'>
+            <h1 className='font-bold text-3xl'>order summery </h1>
+         <p className='pt-8 '>Selected item: {cart.length}</p>
+         <p className='pt-3'>Total price: $ {totalPrize}</p>
+         <p className='pt-3'>Total Shipping Charge: $ {totalShipping}</p>
+         <p className='pt-3'>Tax: {tax}</p>
+         <h6 className='pt-3 font-bold '>Grand Total: ${grandTotal}</h6>
+        </div>
+    );
+};
+
+export default Cart;
