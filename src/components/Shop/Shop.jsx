d96f07d3db8857ffa6dc5c7b47da9dc1c18626e2
Ashik <helloashikul4@gmail.com>
2023-03-29 03:05:03 +0600

    save to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -14,7 +15,15 @@ const Shop = () => {
     const handleCart =(product) =>{
         const newCart =[...cart , product]
         setCart(newCart)
+        addToDb(product.id)
     }
+   
+    useEffect(()=>{
+
+        const shoppingCart = getShoppingCart()
+        console.log(shoppingCart)
+    },[])
+
     return (
         <div className='shop-container '>
             <div className="product-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-6 my-16 ">
