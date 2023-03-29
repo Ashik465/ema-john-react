56c3ba1a970af9be043c2a081f1e1ad498ae1112
Ashik <helloashikul4@gmail.com>
2023-03-29 07:15:58 +0600

    clear-cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { addToDb, getShoppingCart } from "../../utilities/fakedb";
+import { addToDb, deleteShoppingCart, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -32,6 +32,14 @@ const Shop = () => {
     addToDb(product.id);
   };
 
+// delete cart 
+
+const deleteCart =()=>{
+deleteShoppingCart()
+setCart([])
+
+}
+
   useEffect(() => {
     const storedCart = getShoppingCart();
   
@@ -60,12 +68,13 @@ const Shop = () => {
             product={product}
             key={product.id}
             handleCart={handleCart}
+           
           ></Product>
         ))}
       </div>
 
       <div className="cart-container">
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart} deleteCart={deleteCart} ></Cart>
       </div>
     </div>
   );
