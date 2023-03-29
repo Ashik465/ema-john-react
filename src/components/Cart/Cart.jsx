63e457d1202209f799dcef1aee3b2d13cba9cd96
Ashik465 <helloashikul@gmail.com>
2023-03-29 06:39:03 +0600

    showing data to ui from local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 const Cart = ({cart}) => {
     console.log(cart)
 
+    let quantity = 0 ;
     let totalPrize = 0 ;
     let totalShipping = 0;
     for (const product of cart) {
-        totalPrize = totalPrize + product.price 
+    //   product.quantity = product.quantity || 1 
+        totalPrize = totalPrize + product.price* product.quantity
         totalShipping =totalShipping + product.shipping
+        quantity = quantity + product.quantity 
     }
 
     const tax = totalPrize *7/100;
@@ -15,7 +18,7 @@ const Cart = ({cart}) => {
     return (
         <div className='bg-[#FF99004D] p-5 text-lg h-[5%] sticky top-0'>
             <h1 className='font-bold text-3xl'>order summery </h1>
-         <p className='pt-8 '>Selected item: {cart.length}</p>
+         <p className='pt-8 '>Selected item: {quantity}</p>
          <p className='pt-3'>Total price: $ {totalPrize}</p>
          <p className='pt-3'>Total Shipping Charge: $ {totalShipping}</p>
          <p className='pt-3'>Tax: {tax.toFixed(2)}</p>
